0c8522d7c7675e76e1ab9922a753726412724277
janin0318 <janin-ou@outlook.jp>
2024-05-28 22:29:28 +0900

    SpringBootの資材を追加

---
+++ src/test/java/mrs/s3test/S3TestApplicationTests.java
@@ -0,0 +1,13 @@
+package mrs.s3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3TestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
