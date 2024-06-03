7ed62f1ce085aed322296ea08e85896111b7d93a
Yoshiyasu Nio <38271950+janin0318@users.noreply.github.com>
2024-06-03 22:49:12 +0900

    Revert "Add docker (#1)"
    This reverts commit 20785d8438cdf7b57fbabf8c480ee5b529456f95.

--- src/main/java/mrs/s3test/S3TestApplication.java
+++ src/main/java/mrs/s3test/S3TestApplication.java
@@ -1,8 +1,5 @@
 package mrs.s3test;
 
-import mrs.s3test.service.S3Service;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
